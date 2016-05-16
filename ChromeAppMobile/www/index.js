8051e2fe69ced780742819a6eeeaad50ef1dca61
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2016-05-15 22:05:01 -0300

    Adding some support for bluetooth discovering.

--- ChromeAppMobile/www/index.js
+++ ChromeAppMobile/www/index.js
@@ -1,6 +1,6 @@
 var main = (function() {
     function BrightPhotoMedicine() {
-        // A mapping from device addresses to device names for found devices that expose a Battery service.
+        // A mapping from device addresses to device names for found devices that expose a Led service.
         this.deviceMap_ = {};
 
         // The currently selected service and its characteristics.
@@ -11,9 +11,32 @@ var main = (function() {
     }
 
     BrightPhotoMedicine.prototype.init = function () {
+        // Store the |this| to be used by API callbacks below.
         var self = this;
 
+        // Request information about the local Bluetooth adapter to be displayed in the UI.
+        var updateAdapterInfo = function(adapterInfo) {
+            UI.getInstance().setAdapter(adapterInfo.address, adapterInfo.name);
+            self.updateDiscoveryState(adapterInfo.discovering);
+        };
 
+        chrome.bluetooth.getAdapterState(function (adapterState) {
+            if (chrome.runtime.lastError) {
+                console.log(chrome.runtime.lastError.message);
+            }
+
+            self.updateDiscoveryToggleState(adapterState.discovering);
+            updateAdapterInfo(adapterState);
+        });
+
+        chrome.bluetooth.onAdapterStateChanged.addListener(updateAdapterInfo);
+    };
+
+    BrightPhotoMedicine.prototype.updateDiscoveryToggleState = function (discovering) {
+        if (this.discovering_ != discovering) {
+            this.discovering_ = discovering;
+            UI.getInstance().showStatusBluetooth(discovering);
+        }
     };
 
     return {
--- ChromeAppMobile/www/ui.js
+++ ChromeAppMobile/www/ui.js
@@ -6,6 +6,53 @@ var UI = (function () {
 
     // Global instance.
     var instance;
+    
+    // Public methods:
+    
+    UI.prototype.resetState = function() {
+        setFieldValue('status-message', 'Procurando por uma conexão bluetooth com o Arduino...');
+
+        document.getElementById('status-message').hidden = false;
+        document.getElementById('setting-led').hidden = true;
+        
+        this.clearAllFields();  
+    };
+
+    UI.prototype.showStatusBluetooth = function(isOn) {
+        var message = "Procurando por uma conexão bluetooth com o Arduino...";
+
+        if (!isOn) {
+            message = "Por favor, ligue seu bluetooth..."
+        }
+
+        setFieldValue('status-message', message);
+    };
+
+    UI.prototype.setAdapter = function (address, name) {
+        var deviceInfoContainer = document.getElementById('device-info');
+        var addressField = document.getElementById('adapter-address');
+        var nameField = document.getElementById('adapter-name');
+
+        deviceInfoContainer.hidden = true;
+
+        if (address && name) {
+            deviceInfoContainer.hidden = false;
+
+            var setAdapterField = function (field, value) {
+                var textNode = document.createTextNode(value);
+
+                field.innerHTML = '';
+                field.appendChild(textNode);
+            };
+
+            setAdapterField(addressField, address ? address : 'unknown');
+            setAdapterField(nameField, name ? name : 'Local Adapter');
+        }
+    };
+
+    UI.prototype.clearAllFields = function () {
+        
+    };
 
     // Private methods:
 
